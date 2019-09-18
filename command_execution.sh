82fe8fdfd295e1f73708f3f518b15be5857f9330
burnt43 <jms.crsn@gmail.com>
2019-09-18 16:45:48 -0400

    make result local

--- command_execution.sh
+++ command_execution.sh
@@ -1,18 +1,18 @@
 function log_and_execute {
   echo -n "$1..."
 
-  if [ $LOG_AND_EXECUTE_DEBUG == 1 ]; then
-    RESULT=0
+  if [ "$LOG_AND_EXECUTE_DEBUG" == "1" ]; then
+    local result=0
   else
     bash -c "$2 1>/dev/null"
-    RESULT=$?
+    local result=$?
   fi
 
-  if [ $RESULT == 0 ]; then
+  if [ $result == 0 ]; then
     echo -e "\033[0;32mOK\033[0;0m"
   else
     echo -e "\033[0;31mFAIL\033[0;0m"
   fi
 
-  return $RESULT
+  return $result
 }
